86a28ff2ec59a6f9f402f454e2c90b1a541881b7
bgocean <bigocean700@gmail.com>
2024-11-05 02:43:48 +0200

    add lesson8

--- lesson8.py
+++ lesson8.py
@@ -1,3 +1,6 @@
+from mesonbuild.mlog import exception
+
+
 class Human:
     _name: str
     _location: str
@@ -28,6 +31,7 @@ class Children(Human):
     def get_age(self):
         return self._name + ': ' + str(self._age)
 
+
 class Bus:
     __children: list()
 
@@ -42,13 +46,55 @@ class Bus:
             if one_child in self.__children:
                 self.__children.remove(one_child)
 
+    def print_all_children_locations(self):
+        for one_child in self.__children:
+            print(one_child.get_location())
+
+    def go_to_new_location(self, new_location):
+        for one_child in self.__children:
+            one_child.set_location(new_location)
+
 
 first_child = Children()
 second_child = Children('Petya')
 third_child = Children('Igoryok')
-one_child = Children()
-print(one_child.get_location())
-print(one_child.get_age())
+# one_child = Children()
+# print(one_child.get_location())
+# print(one_child.get_age())
+
+one_bus = Bus([first_child, second_child])
+one_bus.print_all_children_locations()
+
+print('-------------------------')
+one_bus.add_children([third_child])
+one_bus.print_all_children_locations()
+
+print('-------------------------')
+# one_bus.remove_children([first_child, second_child])
+one_bus.go_to_new_location('Vinnitsa')
+one_bus.print_all_children_locations()
+
+print('-------------------------')
+one_bus.go_to_new_location('Khmelnitsky')
+one_bus.print_all_children_locations()
+
+######################################################################
+
+list1 = [1, 2, 3, 4, 'hello!', 5, 6, 7]
 
 
-# 19.35
+def list_division(local_list: list):
+    l_result = list()
+    for one_element in local_list:
+        try:
+            l_result.append(100 / (one_element - 2))
+        except ZeroDivisionError:
+            print('На 0 делить нельзя!')
+        except Exception as e:
+            print(e)
+        finally:
+            print(one_element)
+    return l_result
+
+
+print(list_division(list1))
